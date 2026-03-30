6ab1d4a51837320e753bb675f0dae62469bde595
hyon.mota <hyon.mota@mps.com.br>
2026-03-30 11:29:33 -0300

    Adicionado calculo variancia

--- AT0/AT0.s
+++ AT0/AT0.s
@@ -54,22 +54,25 @@ Start
 ; R2: Menor valor (começando em MAX)
 ; R3: Maior valor (començando em 0)
 ; R4: Contador de não nulos
-; R5: Soma total
+; R5: Soma total / Soma das diferenças
 ; R6: Iterador e offset
 ; R7: Valor atual carregado da RAM
 ; R8: Media
 ; R9: Desvio
+; R10: Flag caso já tenha calculado a média
+; R11: Diferenças ao quadrado
+; R12: Carregar valores na RAM / desvio ao quadrado
 
 	MOV R0, #TAMANHO
 	LDR R1, =RAM
 	MOV R2, #MAX
 	
-; Loop principal
-for
-	LDRB R7, [R1, R6]
-	CMP R7, #0
-	BEQ invalido
-	ADDNE R4, #1
+; Função principal(conta não nulos, total e compara maior e menor)
+principal
+	BL valido
+	; Soma a quantidade no total e 1 a quantidade não nula
+	ADD R4, #1
+	ADD R5, R7
 	; Compara se é menor
 	CMP R7, R2
 	IT CC
@@ -78,23 +81,42 @@ for
 	CMP R7, R3
 	IT CS
 		MOVCS R3, R7
+	B iterador
 
-invalido
-	; Soma total
-	ADD R5, R7
+; Compara se é válido, caso seja, volta para função, se não, vai direto para o iterador
+valido
+	LDRB R7, [R1, R6]
+	CMP R7, #0
+	BEQ iterador
+	BX LR
+
+iterador
 	; Adiciona iterador (i++)
 	ADD R6, #1
 	; Duas comparações, quando for menor ou igual
 	CMP R6, R0
-	BCC for
+	BCC voltar
 	CMP R6, R0
-	BEQ for
-	B media
+	BEQ voltar
+	CMP R10, #0
+	BEQ media
+	BNE raiz
 	
+; Decide se vai voltar para função principal ou pra variância
+voltar
+	CMP R10, #0
+	BEQ principal
+	BNE variancia
+
 media
 	UDIV R8, R5, R4
+	; Reseta R6(iterador) e R5(soma das diferenças) para calcular variância e aciona R10(flag)
+	MOV R5, #0
+	MOV R6, #0
+	MOV R10, #1
 	B store
 	
+; Guarda os valores já calculados
 store
 	LDR R12, =MEDIA
 	STRB R8, [R12]
@@ -104,6 +126,20 @@ store
 	STRB R2, [R12]
 	LDR R12, =NOTNULL
 	STRB R4, [R12]
+	B soma_diferencas
+
+soma_diferencas
+	BL valido
+	SUB R11, R7, R8
+	MUL R11, R11, R11 
+	ADD R5, R11
+	B iterador
+
+variancia
+	UDIV R12, R5, R4
+	B raiz
+
+raiz
 	LDR R12, =DESVIO
 	STRB R9, [R12]
 	
