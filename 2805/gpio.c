b8fb9cea9dd26b616ea3abc8d5966cc8c9cd0091
elensoss <elenbeatriz36@gmail.com>
2026-06-18 00:38:37 -0300

    Lab 3

--- 2805/gpio.c
+++ 2805/gpio.c
@@ -7,90 +7,136 @@
 #define GPIO_PORTK  (0x0200) //bit 9
 #define GPIO_PORTM  (0x0800) //bit 11
 #define GPIO_PORTL  (0x0400) //bit 10
+#define GPIO_PORTJ  (0x0100)
+#define GPIO_PORTE  (0x0010)
+#define GPIO_PORTH  (0x0080)
 
 void SysTick_Wait1us(uint32_t delay);
 void SysTick_Wait1ms(uint32_t delay);
 void UART_Init(void);
 
+extern volatile uint8_t flag_timer_200ms;
+
+// configura os pinos de UART (PA), ADC (PE4) e motor (PH0-PH3)
 void GPIO_Init(void)
 {
-	//1a. Ativar o clock para a porta setando o bit correspondente no registrador RCGCGPIO
-	SYSCTL_RCGCGPIO_R = (GPIO_PORTK | GPIO_PORTM | GPIO_PORTL);
-
-	//1b.   após isso verificar no PRGPIO se a porta está pronta para uso.
-  while((SYSCTL_PRGPIO_R & 
-		(GPIO_PORTK | GPIO_PORTM | GPIO_PORTL) ) != 
-		(GPIO_PORTK | GPIO_PORTM | GPIO_PORTL) ){};
+	// liga o clock
+	SYSCTL_RCGCGPIO_R |= (GPIO_PORTK | GPIO_PORTM | GPIO_PORTL | GPIO_PORTJ | GPIO_PORTE | GPIO_PORTH);
+	while((SYSCTL_PRGPIO_R & (GPIO_PORTK | GPIO_PORTM | GPIO_PORTL | GPIO_PORTJ | GPIO_PORTE | GPIO_PORTH)) == 0){};
+		
 	
-	// 2. Limpar o AMSEL para desabilitar a analógica
+	GPIO_PORTH_AHB_AMSEL_R &= ~0x0F;		// desabilita função analógica
+  GPIO_PORTH_AHB_AFSEL_R &= ~0x0F;    // desabilita função alternativa
+  GPIO_PORTH_AHB_PCTL_R  &= ~0xFFFF;  // limpa PCTL
+  GPIO_PORTH_AHB_DIR_R   |= 0x0F;     // configura PH0 a PH3 como saída
+  GPIO_PORTH_AHB_DEN_R   |= 0x0F;     // habilita I/O digital
+	
 	GPIO_PORTK_AMSEL_R = 0x00;
 	GPIO_PORTM_AMSEL_R = 0x00;
 	GPIO_PORTL_AMSEL_R = 0x00;
-		
-	// 3. Limpar PCTL para selecionar o GPIO
 	GPIO_PORTK_PCTL_R = 0x00;
 	GPIO_PORTM_PCTL_R = 0x00;
 	GPIO_PORTL_PCTL_R = 0x00;
 
-	// 4. DIR para 0 se for entrada, 1 se for saída
 	GPIO_PORTK_DIR_R = 0xFF; 
 	GPIO_PORTM_DIR_R = 0xF7;
-	// Saída para motor
 	GPIO_PORTL_DIR_R = 0x10;
 		
-	// 5. Limpar os bits AFSEL para 0 para selecionar GPIO sem função alternativa	
 	GPIO_PORTK_AFSEL_R = 0x00;
 	GPIO_PORTM_AFSEL_R = 0x00;
 	GPIO_PORTL_AFSEL_R = 0x00;
-		
-	// 6. Setar os bits de DEN para habilitar I/O digital	
+
 	GPIO_PORTK_DEN_R = 0xFF;
 	GPIO_PORTM_DEN_R = 0xF7;
-	// Habilidando digital para linhas PL0-PL3 e digital do motor PL4
 	GPIO_PORTL_DEN_R = 0x1F;
 	
-	// 7. Habilitar resistor de pull-up interno, setar PUR para 1
 	GPIO_PORTL_PUR_R = 0x0F;
+	
+	GPIO_PORTE_AHB_AMSEL_R &= ~0x0F;
+	GPIO_PORTE_AHB_AFSEL_R &= ~0x0F;
+	GPIO_PORTE_AHB_PCTL_R  &= ~0x0000FFFF;
+	GPIO_PORTE_AHB_DIR_R   |=  0x0F;
+	GPIO_PORTE_AHB_DEN_R   |=  0x0F;
+	GPIO_PORTE_AHB_AMSEL_R |= 0x10;   
+	GPIO_PORTE_AHB_AFSEL_R |= 0x10;   
+	GPIO_PORTE_AHB_DEN_R   &= ~0x10; 
 }	
 
-
-
+// inicializa UART0 a 57600 baud, paridade impar, 1 stop bit, 8 bits de dados
 void UART_Init(void)
 {
+	SYSCTL_RCGCUART_R |= 0x01;   
+  SYSCTL_RCGCGPIO_R |= 0x01;   
     
-    SYSCTL_RCGCUART_R |= 0x01;   
-    SYSCTL_RCGCGPIO_R |= 0x01;   
-    
-    while((SYSCTL_PRUART_R & 0x01) == 0) {};
-    while((SYSCTL_PRGPIO_R & 0x01) == 0) {};
+  while((SYSCTL_PRUART_R & 0x01) == 0) {};
+  while((SYSCTL_PRGPIO_R & 0x01) == 0) {};
 			
-    UART0_CTL_R &= ~0x00000001; 
+  UART0_CTL_R &= ~0x00000001; 
+ 
+  UART0_IBRD_R = 17; // valor inteiro para 57600 
+  UART0_FBRD_R = 23; // valor fracionário para 57600
+  UART0_LCRH_R = 0x0000006A; // paridade ímpar com 1 stop bit
 
-    UART0_IBRD_R = 86;
-    UART0_FBRD_R = 52;
-    UART0_LCRH_R = 0x00000070;
+  UART0_CC_R = 0;
+  GPIO_PORTA_AHB_AMSEL_R &= ~0x03; 
+  GPIO_PORTA_AHB_AFSEL_R |= 0x03;
+  GPIO_PORTA_AHB_PCTL_R = (GPIO_PORTA_AHB_PCTL_R & 0xFFFFFF00) | 0x00000011;
+  GPIO_PORTA_AHB_DEN_R |= 0x03;
 
-    UART0_CC_R = 0;
-
-    GPIO_PORTA_AHB_AMSEL_R &= ~0x03; 
-    GPIO_PORTA_AHB_AFSEL_R |= 0x03;
-    GPIO_PORTA_AHB_PCTL_R = (GPIO_PORTA_AHB_PCTL_R & 0xFFFFFF00) | 0x00000011;
-    GPIO_PORTA_AHB_DEN_R |= 0x03;
-
-    UART0_CTL_R |= 0x00000301;
+  UART0_CTL_R |= 0x00000301;
 }
 
-void LED_Init(void) {
-    // Ligar clock do Port N (bit 12) e Port F (bit 5)
-    SYSCTL_RCGCGPIO_R |= 0x1020; 
-    while((SYSCTL_PRGPIO_R & 0x1020) != 0x1020) {};
+// configura ADC0 SS3 para leitura do potenciometro no canal AIN9 (PE4) por software
+void ADC_Init(void)
+{
+	SYSCTL_RCGCADC_R |= 0x01; 
+  while((SYSCTL_PRADC_R & 0x01) == 0) {};
 
-    // Configurar LED 1 e 2 (PN1 e PN0)
-    GPIO_PORTN_DIR_R |= 0x03;
-    GPIO_PORTN_DEN_R |= 0x03;
-
-    // Configurar LED 3 e 4 (PF4 e PF0)
-    GPIO_PORTF_AHB_DIR_R |= 0x11;
-    GPIO_PORTF_AHB_DEN_R |= 0x11;
+  ADC0_PC_R = 0x07; 
+	ADC0_SSPRI_R = 0x0123; 
+	ADC0_ACTSS_R &= ~0x0008; 
+  ADC0_EMUX_R &= ~0xF000; 
+  ADC0_SSMUX3_R = (ADC0_SSMUX3_R & ~0x000F) | 0x0009; 
+  ADC0_SSCTL3_R = 0x0006; 
+	ADC0_ACTSS_R |= 0x0008; 
 }
 
+// dispara uma conversao AD e retorna o resultado de 12 bits (0 a 4095)
+uint32_t ADC0_ReadSS3(void)
+{
+	uint32_t valor_convertido;
+  ADC0_PSSI_R = 0x0008;
+	
+	while((ADC0_RIS_R & 0x0008) == 0) {};
+  valor_convertido = ADC0_SSFIFO3_R & 0x0FFF;
+
+  ADC0_ISC_R = 0x0008;
+
+  return valor_convertido; 
+}
+
+// envia o padrao de bits para as bobinas do motor via PH0-PH3 com logica invertida (ULN2003)
+void Motor_Output(uint32_t valor) {
+    uint32_t temp = GPIO_PORTH_AHB_DATA_R & ~0x0F; // mantém os bits 4 a 7 intactos
+    temp = temp | (~valor & 0x0F);                 // aplica o valor invertido nos bits 0 a 3
+    GPIO_PORTH_AHB_DATA_R = temp;
+}
+
+// configura Timer0A para gerar interrupcao periodica a cada 200ms
+void Timer0A_Init(void) {
+    SYSCTL_RCGCTIMER_R |= 0x01;         // clock no Timer0
+    while((SYSCTL_PRTIMER_R & 0x01) == 0){};
+    TIMER0_CTL_R    &= ~0x01;           // desabilita Timer0A
+    TIMER0_CFG_R     = 0x00;            // 32 bits
+    TIMER0_TAMR_R    = 0x02;            // periódico
+    TIMER0_TAILR_R   = 15999999;    
+    TIMER0_IMR_R    |= 0x01;            // habilita interrupção
+    NVIC_EN0_R      |= (1 << 19);       // habilita IRQ19 (Timer0A)
+    TIMER0_CTL_R    |= 0x01;            // habilita Timer0A
+}
+
+// ISR do Timer0A: limpa a flag de interrupcao e sinaliza para a main que passaram 200ms
+void Timer0A_Handler(void) {
+    TIMER0_ICR_R = 0x01;
+    flag_timer_200ms = 1;
+}
--- 2805/main.c
+++ 2805/main.c
@@ -1,6 +1,6 @@
 // main.c
 // Desenvolvido para a placa EK-TM4C1294XL
-// LAB 2 - Controle de Posicao do Servomotor
+// LAB 3 - SISTEMA DE POSICIONAMENTO COM MOTOR DE PASSO
 // Prof. Guilherme Peron
 
 #include <stdint.h>
@@ -15,78 +15,201 @@ void GPIO_Init(void);
 void LCD_EnviaComando(uint32_t comando);
 void LCD_EnviaDado(uint32_t dado);
 void LCD_EnviaString(const char *string);
-void LED_Init(void);
 void UART_Init(void);
 char UART_InChar(void);
 void UART_OutChar(char data);
 void UART_OutString(char *pt);
+void ADC_Init(void);
+uint32_t ADC0_ReadSS3(void);
+void Timer0A_Init(void);
+void Timer0A_Handler(void);
+char UART_InCharNonBlocking(void);
+void Motor_Output(uint32_t valor);
+void EnableInterrupts(void);
+void UART_PrintUInt(uint32_t valor);
+
+// mapeamento para ativação das bobinas
+const uint8_t seq_passo_completo[4] = {0x03, 0x06, 0x0C, 0x09}; 
+const uint8_t seq_meio_passo[8]     = {0x01, 0x03, 0x02, 0x06, 0x04, 0x0C, 0x08, 0x09};
+
+volatile uint8_t flag_timer_200ms = 0;
+
+// definição dos estados
+typedef enum {
+    ESTADO_IDLE,
+    ESTADO_POTENCIOMETRO,
+    ESTADO_TERMINAL
+} EstadosSistema;
+
 
 int main(void)
 {
-	char comando;
+	int32_t posicao_atual_passos = 0;
+  int32_t passos_alvo = 0;
+  uint8_t indice_motor_meiopasso = 0;
+	static uint8_t contador_1s = 0;
+	
+	// inicialização
 	PLL_Init();
 	SysTick_Init();
 	GPIO_Init();
-	LED_Init();
 	UART_Init();
+	ADC_Init();
+	Timer0A_Init();
+	EnableInterrupts();
 	
+	UART_OutString("Sistema em IDLE. Esperando comando...\r\n");
+	
+	EstadosSistema estado_atual = ESTADO_IDLE;
 	while(1) {
-		comando = UART_InChar(); 
-
-    switch(comando) {
-			case '1':
-				GPIO_PORTN_DATA_R ^= 0x02; //inverte o estado
-        if(GPIO_PORTN_DATA_R & 0x02) 
-					UART_OutString("led 1 aceso\r\n");
-        else 
-          UART_OutString("led 1 apagado\r\n");
-			  break;
-
-			case '2':
-				GPIO_PORTN_DATA_R ^= 0x01;
-        if(GPIO_PORTN_DATA_R & 0x01) 
-					UART_OutString("led 2 aceso\r\n");
-        else 
-          UART_OutString("led 2 apagado\r\n");
-        break;
-
-			case '3':
-				GPIO_PORTF_AHB_DATA_R ^= 0x10; 
-        if(GPIO_PORTF_AHB_DATA_R & 0x10) 
-					UART_OutString("led 3 aceso\r\n");
-        else 
-					UART_OutString("led 3 apagado\r\n");
-        break;
-
-      case '4':
-				GPIO_PORTF_AHB_DATA_R ^= 0x01; 
-        if(GPIO_PORTF_AHB_DATA_R & 0x01) 
-					UART_OutString("led 4 aceso\r\n");
-        else 
-          UART_OutString("led 4 apagado\r\n");
-        break;
+		
+		// verifica qual tecla foi apertada sem travar o motor
+		char tecla = UART_InCharNonBlocking();    
+		// seleção de modos
+    if (tecla == 'p' || tecla == 'P') {
+			estado_atual = ESTADO_POTENCIOMETRO;
+    }
+		else if (tecla == 't' || tecla == 'T') {
+      estado_atual = ESTADO_TERMINAL;
+    }
+		
+    // MODO POTENCIÔMETRO
+    if (estado_atual == ESTADO_POTENCIOMETRO) {
+			// a leitura acontece quando a flag do timer sobe
+      if (flag_timer_200ms) {
+				flag_timer_200ms = 0;
+				uint32_t adc_val = ADC0_ReadSS3();
+				passos_alvo = adc_val;
+				contador_1s++;
+				if (contador_1s >= 5) {
+					contador_1s = 0;
+					int32_t graus_x10 = (posicao_atual_passos * 3600) / 4095;
+					UART_OutString("[POT] ADC: ");
+					UART_PrintUInt(adc_val);
+					UART_OutString(" | Posicao: ");
+					UART_PrintUInt(graus_x10 / 10);
+					UART_OutChar(',');
+					UART_PrintUInt(graus_x10 % 10);
+					UART_OutString(" graus\r\n");
+				}
+			}
+      // a cada loop o motor anda meio passo
+      if (posicao_atual_passos != passos_alvo) {
+				if (passos_alvo > posicao_atual_passos) {
+					indice_motor_meiopasso = (indice_motor_meiopasso + 1) % 8; // cíclico
+          posicao_atual_passos++;
+        } else {
+					indice_motor_meiopasso = (indice_motor_meiopasso - 1);
+          if (indice_motor_meiopasso > 7) indice_motor_meiopasso = 7; // caso tenha overflow
+          posicao_atual_passos--;
+				}       
+        Motor_Output(seq_meio_passo[indice_motor_meiopasso]);       
+        SysTick_Wait1ms(5); 
+      }
 		}
-	}
+        
+		// MODO TERMINAL
+		else if (estado_atual == ESTADO_TERMINAL) {
+			static char cmd_buf[5] = {0};
+			static uint8_t cmd_idx = 0;
+			if (tecla != 0) {
+				UART_OutChar(tecla); // eco
+				// assim que aperta enter, verifica se tem 4 dígitos e converte de graus para passos
+				if (tecla == '\r' || tecla == '\n') {
+					if (cmd_idx == 4 && (cmd_buf[0] == '+' || cmd_buf[0] == '-')) {
+						int32_t graus = (cmd_buf[1]-'0')*100 + (cmd_buf[2]-'0')*10 + (cmd_buf[3]-'0');
+						int32_t passos = (int32_t)((graus * 2048L) / 360);
+						if (cmd_buf[0] == '-') passos = -passos;
+							passos_alvo = posicao_atual_passos + passos;
+					}
+					cmd_idx = 0;
+				} else if (cmd_idx < 4) {
+					cmd_buf[cmd_idx++] = tecla;
+				}
+			}
+			// print periodico a cada 1 segundo
+			if (flag_timer_200ms) {
+				flag_timer_200ms = 0;
+				contador_1s++;
+				if (contador_1s >= 5) {
+					contador_1s = 0;
+					int32_t graus_x10 = (posicao_atual_passos * 3600) / 4095;
+					UART_OutString("[TERM] Posicao: ");
+					UART_PrintUInt(graus_x10 / 10);
+					UART_OutChar(',');
+					UART_PrintUInt(graus_x10 % 10);
+					UART_OutString(" graus\r\n");
+				}
+			}
+
+			// a cada loop anda um passo inteiro
+			if (posicao_atual_passos != passos_alvo) {
+				if (passos_alvo > posicao_atual_passos) {
+					indice_motor_meiopasso = (indice_motor_meiopasso + 1) % 4;
+					posicao_atual_passos++;
+				} else {
+					indice_motor_meiopasso = (indice_motor_meiopasso + 3) % 4;
+					posicao_atual_passos--;
+				}
+				Motor_Output(seq_passo_completo[indice_motor_meiopasso % 4]);
+				SysTick_Wait1ms(5);
+				if (posicao_atual_passos == passos_alvo) {
+					int32_t graus_x10 = (posicao_atual_passos * 3600) / 4095;
+					UART_OutString("[TERM] Posicao: ");
+					UART_PrintUInt(graus_x10 / 10);
+					UART_OutChar(',');
+					UART_PrintUInt(graus_x10 % 10);
+					UART_OutString(" graus\r\n");
+				}
+			}
+		}
+  }
 }
 
-// Lê um caractere vindo do terminal (Bloqueante)
+// lê um caractere vindo do terminal 
 char UART_InChar(void) {
-    // Espera até que o flag RXFE (Receive FIFO Empty) seja 0 (ou seja, chegou dado)
     while((UART0_FR_R & 0x0010) != 0);
     return (char)(UART0_DR_R & 0xFF);
 }
 
-// Envia um caractere para o terminal
+// envia um caractere para o terminal
 void UART_OutChar(char data) {
-    // Espera até que o flag TXFF (Transmit FIFO Full) seja 0 (ou seja, FIFO tem espaço)
     while((UART0_FR_R & 0x0020) != 0);
     UART0_DR_R = data;
 }
 
-// Envia uma string completa para o terminal
+// envia uma string para o terminal
 void UART_OutString(char *pt) {
     while(*pt) {
         UART_OutChar(*pt);
         pt++;
+    }
+}
+
+// lÊ um caractere vindo do terminal sem bloquear o motor
+char UART_InCharNonBlocking(void) {
+    if((UART0_FR_R & 0x0010) == 0) {
+        return (char)(UART0_DR_R & 0xFF);
+    }
+    return 0;
+}
+
+void UART_PrintUInt(uint32_t valor)
+{
+    uint8_t digits[10];
+    uint8_t count = 0;
+
+    if (valor == 0) {
+        UART_OutChar('0');
+        return;
+    }
+
+    while (valor > 0) {
+        digits[count++] = valor % 10;
+        valor /= 10;
+    }
+
+    for (int8_t i = count - 1; i >= 0; i--) {
+        UART_OutChar('0' + digits[i]);
     }
 }
